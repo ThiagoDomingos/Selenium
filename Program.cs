d527117ce5c06d9cee9c99bd8b658fde815baf30
Basu <Basu@DESKTOP-BUC9V6V>
2023-09-25 23:33:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium 
+{ 
+    class Program
+    {
+        
+
+        public static void Main( )
+        {
+            //Create the reference of browser - GLOBAL
+            IWebDriver driver = new ChromeDriver();
+
+            //Example to Navigate
+            driver.Navigate().GoToUrl("https://app.endtest.io/guides/docs/how-to-test-dropdowns/");
+
+            //Find the Element
+            //IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Usando metodos da classe SeleniumSetMethods
+            
+            //EnterText OK
+            //SeleniumSetMethods.EnterText(driver, "APjFqb", "testando", "Id");
+
+            //SelectDropDown OK
+            SeleniumSetMethods.SelectDropDown(driver, "pets", "Dog", "Id");
+
+            //Click
+            SeleniumSetMethods.Click(driver, "[class='docs-next button']", "Selector");
+            
+
+            //Perform Ops sem classe
+            //element.SendKeys("teste");
+
+            driver.Quit();
+
+            
+
+      
+        }
+
+    }
+
+}
---
+++ SeleniumSetMethods.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(IWebDriver driver, string atributevalue, string value, string atributetype)
+        {
+            if (atributetype == "Id")
+                driver.FindElement(By.Id(atributevalue)).SendKeys(value);
+            if (atributetype == "Name")
+                driver.FindElement(By.Name(atributevalue)).SendKeys(value);
+        }
+
+        //Click button, checkbox, option etc
+        public static void Click(IWebDriver driver, string atributevalue, string atributetype)
+        {
+            if (atributetype == "Id")
+                driver.FindElement(By.Id(atributevalue)).Click();
+            if (atributetype == "Name")
+                driver.FindElement(By.Name(atributevalue)).Click();
+            if (atributetype == "Selector")
+                driver.FindElement(By.CssSelector(atributevalue)).Click();
+        }
+
+        //Select a drop down control
+        public static void SelectDropDown(IWebDriver driver, string atributevalue, string value, string atributetype)
+        {
+            
+            if (atributetype == "Id")
+                new SelectElement( driver.FindElement(By.Id(atributevalue))).SelectByText(value);
+            if (atributetype == "Name")
+                new SelectElement(driver.FindElement(By.Name(atributevalue))).SelectByText(value);
+        }
+
+    }
+}
